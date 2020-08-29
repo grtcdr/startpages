f570c65f7248d5d9cd8b3925d4d1b24d2d87c36b
grtcdr <ba.tahaaziz@gmail.com>
2020-08-29 20:03:41 +0100

    startpage_2 now remembers your last mode + visual changes + extension update 1.0.4

--- startpage_2/src/script.js
+++ startpage_2/src/script.js
@@ -1,65 +1,69 @@
 // Change this to your username
 var username = "grtcdr";
-var mode = "dark";
+
+if (localStorage.getItem('data') == null) var mode = "dark";
+else mode = localStorage.getItem('data');
 
 const determineGreet = hours => document.getElementById("greeting").innerText = `Good ${hours < 12 ? "Morning," : hours < 18 ? "Afternoon," : "Evening,"} ${username}.`;
 
 // The same as "onload"
 window.addEventListener('load', (event) => {
     let today = new Date();
-    let time = today.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+    let time = today.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    change_colors(mode);
     determineGreet(new Date().getHours());
     displayTime(time);
-  });
+});
 
-setInterval( function() {
+setInterval(function () {
     var today = new Date();
-    var time = today.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+    var time = today.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
     document.getElementById("time").innerHTML = time;
 }, 1000);
 
-document.getElementById("mode").addEventListener("click", function(){
-    if (mode == "dark") { 
-        change_colors(mode);
-        mode = "light";
+document.getElementById("mode").addEventListener("click", function () {
+    if (mode == "dark") {
+        change_colors("light");
+        localStorage.setItem('data', 'light');
+        data = localStorage.getItem('data');
+        mode = data;
     }
-    else { 
-        change_colors(mode);
-        mode = "dark"
+    else {
+        change_colors("dark");
+        localStorage.setItem('data', 'dark');
+        data = localStorage.getItem('data');
+        mode = data;
     }
-    
-    console.log(mode);
-}); 
+});
 
-function displayTime(time) 
-{
+function displayTime(time) {
     document.getElementById("time").innerHTML = time;
 }
 
-function dark_colors()
-{
+function dark_colors() {
     let link = document.getElementsByTagName('a');
-    document.body.style.background="#0C131F";
+    let mode_id = document.getElementById("mode");
+    document.body.style.background = "#0C131F";
     document.getElementById("greeting").style.color = "#dee3de";
-    document.getElementById("mode").style.filter = "invert(100%) sepia(0%) saturate(1620%) hue-rotate(8deg) brightness(94%) contrast(88%)";
-    for (i=0;i<link.length;i++) {
+    mode_id.src = "ico/light.png";
+    mode_id.style.filter = "invert(100%) sepia(0%) saturate(1620%) hue-rotate(8deg) brightness(94%) contrast(88%)";
+    for (i = 0; i < link.length; i++) 
         link[i].style.color = "#bbb";
-    }
+
 }
-function light_colors()
-{
+
+function light_colors() {
     let link = document.getElementsByTagName('a');
+    let mode_id = document.getElementById("mode");
     document.body.style.background = "#FFE8D4";
-    document.getElementById("mode").style.filter = "invert(58%) sepia(12%) saturate(482%) hue-rotate(148deg) brightness(94%) contrast(86%)";
     document.getElementById("greeting").style.color = "#767283";
-    for (i=0;i<link.length;i++) {
-    link[i].style.color = "#767283";
-    }
+    mode_id.src = "ico/dark.png";
+    mode_id.style.filter = "invert(58%) sepia(12%) saturate(482%) hue-rotate(148deg) brightness(94%) contrast(86%)";
+    for (i = 0; i < link.length; i++) 
+        link[i].style.color = "#767283";
 }
 
-function change_colors(mode)
-{
-    if (mode == "dark") { light_colors(); }
-    else { dark_colors(); }
+function change_colors(mode) {
+    if (mode == "dark") light_colors();
+    else dark_colors();
 }
-
