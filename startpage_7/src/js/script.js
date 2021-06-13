b1918c71b7c5f18737e6ed1f4ce90cb0eff05717
Aziz Ben Ali <ba.tahaaziz@gmail.com>
2021-06-13 11:08:09 +0100

    startpage_7: omit useless variable

--- startpage_7/src/js/script.js
+++ startpage_7/src/js/script.js
@@ -7,7 +7,6 @@ const determineGreet = hours => document.getElementById("greeting").innerText = 
 // The page will determine what greeting to display depending
 // on the time of day, as soon as it loads.
 window.addEventListener('load', () => {
-    let today = new Date();
     determineGreet(new Date().getHours());
     setTimeout(() => { document.getElementById("greeting").style.opacity = "1"; }, 150);
 });
